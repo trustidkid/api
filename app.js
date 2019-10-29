d023684dae7d6be166758746b962167ca46e5dc6
trustidkid <yemi.bili07@gmail.com>
2019-10-29 10:40:00 +0100

    modified order endpoints

--- app.js
+++ app.js
@@ -24,60 +24,102 @@ if(err)throw err;
 });*/
 
 
-app.post("/new_order",(req, res)=>{
-    const order = req.body;
-    if(order.food_name && order.food_qty && order.customer_name){
-        orders.push({...order,
-        id: `${order.length + 1}`,
+app.post("/new-order", (req, res) => {
+    
+    const { food_name, customer_name, food_qty } = req.body; //Object.entries(req.query);
+    
+    console.log(req.body);
+    //if (order.food_name && order.customer_name && order.food_qty) {
+        if(food_name && customer_name && food_qty){
+      orders.push({ food_name,customer_name,food_qty,
+      //  ...order,
+  
+        id: `${orders.length + 1}`,
+  
         date: Date.now().toString()
-        });
-        res.status(200).json({
-            message: "Order was successful"
-        });
-    }else{
+      });
+
+      res.status(200).json({
+        message: "Order created successfully"
+      });
+    } else {//return res.status().json();
         res.status(401).json({
-            message: "Invalid order creation"
-        })
+        message: "Invalid Order creation"
+      });
     }
-  })
-
-  app.get("/get_orders", (req, res)=>{
-      res.status(200).send(orders);
-
   });
 
-  app.patch("/update_order/:id",(req, res)=>{
-      const order_id = req.param.id;
-      const order_update = req.body;
+  /*
+  Get the all the list of order
+   */
+  app.get("/get-orders",(req, res)=>{
+      if(orders.length !== null || undefined)
+      res.status(200).send(orders);
+  })
+  /*
+  Get order by the order Id number
+  */
+  app.get("/get-order/:id",(req, res)=>{
+      const id = req.params.id;
+      if (id === ""){
+        res.status(404).json({message: "Order Id not exist"});
+      }
+      //let [food_name, customer_name, food_qty] = Object.entries(req.query);
+      console.log("Value of " + `${id}`);
+      console.log("Number of Orders: " + `${orders.length}`);
+      let orderRequest = "";
+      for(let order of orders){
+            orderRequest = {
+                "food_name": order.food_name,
+                "customer_name": order.customer_name,
+                "food_qty": order.food_qty
+          }
+      }
+    return res.status(200).json({ message: orderRequest});
+})
+
+  app.put("/update-order/:id",(req, res)=>{
+      const order_id = req.params.id;
+      //let [ food_name, customer_name, food_qty ] = Object.entries(req.body);
+      const { food_name, customer_name, food_qty } = req.body;
+    
+      console.log("Number of orders: " + orders);
 
       for(let order of orders){
         if(order.id === order_id){
-            if(order_update.food_name !== null || undefined)
-                order.food_name = order_update.food_name;
+            if(food_name !== null || undefined)
+                order.food_name = food_name;
+
+            if(food_qty !== null || undefined)
+                order.food_qty = food_qty;
             
-            if(order_update.food_qty !== null || undefined)
-                order.food_qty = order_update.food_qty;
-            
-            if(order_update.customer_name !== null || undefined)
-                order.customer_name = order_update.customer_name;
-            
-           return res.status(200).json({ message: "Order updated successfully"})
+            if(customer_name !== null || undefined)
+                order.customer_name = customer_name;
+        }
+        else{
+         //   res.status(404).json({ message: "Invalid order Id supplied "});
         }
       }
-      res.status(404).json({ message: "Invalid order Id updated successfully"})   
+      return res.status(200).json({ message: "Order updated successfully"})
+    
   })
 
-  app.delete("/delete_order/:id",(req, res)=>{
+  app.delete("/delete-order/:id",(req, res)=>{
 
-    const order_id = req.param.id;
-    const order_delete = req.body;
+    const id = req.params.id;
+    if(id === ""){
+        res.json({message: "Please provide order number"});
+    }
 
     for (let order of orders){
-        if(order.id === order_id)
-        orders.slice(orders.indexOf(order),1);
-        return res.status(200).json({message: "Order deleted"})
+        if(order.id === id){
+            orders.splice(orders.indexOf(order),1);
+        }else{
+            res.status(404).json({message: "Invalid Order ID"})
+        }
     }
-    res.status(404).json({message: "Invalid Order "})
+    return res.status(200).json({message: "Order deleted"});
+
   });
 
   app.listen(port, () => {
